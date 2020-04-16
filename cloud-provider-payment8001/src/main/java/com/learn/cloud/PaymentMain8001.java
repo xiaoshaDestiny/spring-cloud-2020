188fb9b8f05e1ffa30eabd9c9ce12ab69c11fec1
xiaoshaDestiny <2284420047@qq.com>
2020-04-16 21:12:34 +0800

    8001支付模块服务构建

---
+++ cloud-provider-payment8001/src/main/java/com/learn/cloud/PaymentMain8001.java
@@ -0,0 +1,15 @@
+package com.learn.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xrb
+ * @since 2020-04-16 21:09
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
