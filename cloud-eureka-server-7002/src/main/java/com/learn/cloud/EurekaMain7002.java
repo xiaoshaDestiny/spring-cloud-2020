2c6e0f311fe609f9df7b353df2fdd620bf37fb5b
xiaoshaDestiny <2284420047@qq.com>
2020-04-17 21:43:41 +0800

    7002 多个注册中心的eureka集群

---
+++ cloud-eureka-server-7002/src/main/java/com/learn/cloud/EurekaMain7002.java
@@ -0,0 +1,18 @@
+package com.learn.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author xrb
+ * @create 2020-04-17 21:30
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class,args);
+    }
+
+}
