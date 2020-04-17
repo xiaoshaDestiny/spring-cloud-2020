2c651ffc1ff30b0f29bc9f0b68e4b77012cb407d
xiaoshaDestiny <2284420047@qq.com>
2020-04-17 21:03:26 +0800

    8001 服务提供者注册到 Eureka

--- README.md
+++ README.md
@@ -12,5 +12,6 @@ Eureka现在官网已经停止更新。
 可以实现服务调用、负载均衡、服务注册与发现。
 Eureka作为服务注册功能的服务区，它是服务的注册中心。系统中其他的微服务，使用Eureka的客户端连接到Eureka Server并且为之心跳连接。就可以通过Eureka Server来监控系统中各个微服务是否正常运行。
 服务把把服务地址、通讯地址以别名的形式方式注册到注册中心上。
-@EnableEurekaServer 标注在注册中心server上  
+@EnableEurekaServer 标注在注册中心server上
+@EnableEurekaClient 标注在服务提供者上  
 
--- cloud-provider-payment8001/src/main/java/com/learn/cloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/learn/cloud/PaymentMain8001.java
@@ -2,12 +2,14 @@ package com.learn.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author xrb
  * @since 2020-04-16 21:09
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class,args);
