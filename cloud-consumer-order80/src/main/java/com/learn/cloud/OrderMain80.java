fce88601092097aa01e3081f684666676a219745
xiaoshaDestiny <2284420047@qq.com>
2020-04-17 21:22:29 +0800

    80 服务消费者注册到 Eureka

--- cloud-consumer-order80/src/main/java/com/learn/cloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/learn/cloud/OrderMain80.java
@@ -2,12 +2,14 @@ package com.learn.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author xrb
  * @since 2020-04-17 12:41
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class,args);
