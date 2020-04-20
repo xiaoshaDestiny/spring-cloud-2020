9f292544c37bbaa261c504138fe08badc1d52658
xiaoshaDestiny <2284420047@qq.com>
2020-04-20 13:11:00 +0800

    hystrix 8001 服务提供者配置降级处理

--- cloud-provider-hystrix-payment8001/src/main/java/com/learn/cloud/PaymentHystrixMain8001.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/learn/cloud/PaymentHystrixMain8001.java
@@ -2,6 +2,7 @@ package com.learn.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
@@ -11,6 +12,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 public class PaymentHystrixMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentHystrixMain8001.class,args);
--- cloud-provider-hystrix-payment8001/src/main/java/com/learn/cloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/learn/cloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.learn.cloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -25,15 +27,25 @@ public class PaymentService {
      * @param id
      * @return
      */
+    //设置自身调用超时的时间峰值，峰值之内可以正常运行，超时要有兜底的方法处理，作为服务降级fallback
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler",commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "3000")
+    })
     public String paymentInfoTimeOut(Integer id){
-        int time = 3;
+
+        int testWrong = 10/0;
+        int time = 5;
         try {
             TimeUnit.SECONDS.sleep(time);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         return "线程池： "+ Thread.currentThread().getName()+" paymentInfo_TimeOut,id: "+id+" 耗时"+time+"秒钟";
+    }
 
+
+    public String paymentInfo_TimeOutHandler(Integer id){
+        return "线程池： "+ Thread.currentThread().getName()+" paymentInfo_TimeOutHandler,id: "+id+"系统繁忙，稍后重试！ o...o ";
     }
 
 
