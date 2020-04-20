6f8aaa98c50c4921afbf23acbdf97f7cc0c6d81f
xiaoshaDestiny <2284420047@qq.com>
2020-04-20 13:30:33 +0800

    hystrix 80 服务消费者配置降级处理

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/learn/cloud/OrderHystrixMain80.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/learn/cloud/OrderHystrixMain80.java
@@ -3,6 +3,7 @@ package com.learn.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -12,6 +13,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableHystrix
 public class OrderHystrixMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderHystrixMain80.class,args);
--- cloud-consumer-feign-hystrix-order80/src/main/java/com/learn/cloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/learn/cloud/controller/OrderHystrixController.java
@@ -1,6 +1,8 @@
 package com.learn.cloud.controller;
 
 import com.learn.cloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,12 +21,20 @@ public class OrderHystrixController {
     private PaymentHystrixService paymentHystrixService;
 
     @GetMapping("/consumer/payment/hystrix/ok/{id}")
-    String paymentInfo_OK(@PathVariable("id") Integer id){
+    public String paymentInfo_OK(@PathVariable("id") Integer id){
         return paymentHystrixService.paymentInfo_OK(id);
     }
 
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
-    String paymentInfo_TimeOut(@PathVariable("id") Integer id){
+    @HystrixCommand(fallbackMethod = "paymentTimeOutFallBackMethod",commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "1500")
+    })
+    public String paymentInfo_TimeOut(@PathVariable("id") Integer id){
+        int i = 10/0;
         return paymentHystrixService.paymentInfo_TimeOut(id);
+    }
+
+    public String paymentTimeOutFallBackMethod(@PathVariable("id") Integer id){
+        return "我是消费者80,对方支付系统繁忙，请10秒之后再次尝试。（000||000） id = " + id;
     }
 }
