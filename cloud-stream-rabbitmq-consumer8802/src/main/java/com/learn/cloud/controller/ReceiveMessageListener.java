eeed3c41011102a0bb449277c33e4ca8c9bf90a7
xiaoshaDestiny <2284420047@qq.com>
2020-04-23 00:30:15 +0800

    8802 stream 消息消费者

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/learn/cloud/controller/ReceiveMessageListener.java
@@ -0,0 +1,28 @@
+package com.learn.cloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author xu.rb
+ * @since 2020-04-23 0:19
+ */
+@Slf4j
+@Component
+@EnableBinding(Sink.class)
+public class ReceiveMessageListener {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @StreamListener(Sink.INPUT)
+    public void input(Message<String> message){
+        log.info("消费者是 serverPort: "+serverPort+"   ---   message是:" + message.getPayload());
+    }
+}
