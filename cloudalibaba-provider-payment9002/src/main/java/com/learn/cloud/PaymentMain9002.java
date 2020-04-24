a7d07e1f0f2554f67bf5c5461d1a82c0e3165b41
xiaoshaDestiny <2284420047@qq.com>
2020-04-24 15:49:52 +0800

    9002 服务提供者 使用Nacos作为服务注册中心

---
+++ cloudalibaba-provider-payment9002/src/main/java/com/learn/cloud/PaymentMain9002.java
@@ -0,0 +1,18 @@
+package com.learn.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author xu.rb
+ * @since 2020-04-24 15:38
+ */
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9002 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9002.class,args);
+    }
+}
