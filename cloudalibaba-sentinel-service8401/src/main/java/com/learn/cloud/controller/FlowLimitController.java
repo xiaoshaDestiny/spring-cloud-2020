464b6ccba25b9d845f007f7bf2ff133facb362ee
xiaoshaDestiny <2284420047@qq.com>
2020-04-26 21:18:32 +0800

    Sentinel 流控 热点规则

--- cloudalibaba-sentinel-service8401/src/main/java/com/learn/cloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/learn/cloud/controller/FlowLimitController.java
@@ -1,7 +1,10 @@
 package com.learn.cloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -20,4 +23,25 @@ public class FlowLimitController {
     public String getB(){
         return "-------- testB";
     }
+
+    @GetMapping("/testD")
+    public String getD(){
+        return "-------- testD";
+    }
+
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler = "dealTestHotKey")
+    public String getHotKey(@RequestParam(value = "p1",required = false) String p1,
+                            @RequestParam(value = "p2",required = false) String p2){
+        return "-------- testHotKey  p1:"+p1+"  p2:"+p2;
+    }
+
+
+    public String dealTestHotKey(String p1, String p2, BlockException exception){
+
+        return "dealTestHotKey......";
+    }
+
+
 }
