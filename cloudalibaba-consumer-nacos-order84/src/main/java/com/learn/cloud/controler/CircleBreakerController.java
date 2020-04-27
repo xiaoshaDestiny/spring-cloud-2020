a230e31da78da0667a3777df54bbb453fe5c92cf
xiaoshaDestiny <2284420047@qq.com>
2020-04-27 15:55:37 +0800

    @SentinelResource 注解 指定 fallback 和 blockHandler

--- cloudalibaba-consumer-nacos-order84/src/main/java/com/learn/cloud/controler/CircleBreakerController.java
+++ cloudalibaba-consumer-nacos-order84/src/main/java/com/learn/cloud/controler/CircleBreakerController.java
@@ -1,6 +1,7 @@
 package com.learn.cloud.controler;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.learn.cloud.entities.CommonResult;
 import com.learn.cloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
@@ -24,10 +25,13 @@ public class CircleBreakerController {
     @Resource
     private RestTemplate restTemplate;
 
-    @RequestMapping("/consumer/fallback/{id}")
-    @SentinelResource(value = "fallback")
-    public CommonResult<Payment> fallback(@PathVariable("id") Long id){
 
+    @RequestMapping("/consumer/fallback/{id}")
+    //@SentinelResource(value = "fallback")//什么都不配置
+    //@SentinelResource(value = "fallback",fallback = "handlerFallback")//只负责业务上的异常
+    //@SentinelResource(value = "fallback",blockHandler = "blockHandler")//只配置阻塞异常，需要配置降级规则，才能跑到兜底的方法上
+    @SentinelResource(value = "fallback",fallback = "handlerFallback", blockHandler = "blockHandler")//两种都配置  各回各家
+    public CommonResult<Payment> fallback(@PathVariable("id") Long id){
         CommonResult result = restTemplate.getForObject(SERVICE_URL + "/paymentSQL/" + id, CommonResult.class, id);
         if(id == 4){
             throw  new IllegalArgumentException("IllegalArgumentException  非法参数异常.....");
@@ -36,5 +40,29 @@ public class CircleBreakerController {
             throw new NullPointerException("NullPointerException ,没有对应ID的记录，空指针异常");
         }
         return  result;
+    }
+
+
+
+    /**
+     * 业务员兜底fallback
+     * @param id
+     * @param e
+     * @return
+     */
+    public CommonResult handlerFallback(@PathVariable("id") Long id,Throwable e){
+        Payment payment = new Payment(id,null);
+        return new CommonResult(444,"兜底业务异常，exception is:"+e.getMessage(),payment);
+    }
+
+    /**
+     * 请求上的限流
+     * @param id
+     * @param blockException
+     * @return
+     */
+    public CommonResult blockHandler(@PathVariable("id") Long id,BlockException blockException){
+        Payment payment = new Payment(id,null);
+        return new CommonResult(445,"请求被限流，blockException is:" + blockException.getMessage(),payment);
     }
 }
