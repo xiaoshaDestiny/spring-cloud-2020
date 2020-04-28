fc28c1c169aa086eee016069e637d91327da4636
xiaoshaDestiny <2284420047@qq.com>
2020-04-28 16:12:07 +0800

    Seata 处理分布式事务 2001 订单服务

--- seata-order-service2001/src/main/java/com/learn/cloud/service/AccountService.java
+++ seata-order-service2001/src/main/java/com/learn/cloud/service/AccountService.java
@@ -1,9 +1,11 @@
 package com.learn.cloud.service;
 
+import org.springframework.cloud.openfeign.FeignClient;
+
 /**
  * @author xu.rb
- * @description: TODO
  * @since 2020-04-28 15:37
  */
+@FeignClient("seata-account-service")
 public interface AccountService {
 }
