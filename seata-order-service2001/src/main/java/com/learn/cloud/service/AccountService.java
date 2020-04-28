f076eff34edbb8e504b3111caca7cc83e86d80ba
xiaoshaDestiny <2284420047@qq.com>
2020-04-28 17:26:47 +0800

    Seata 完整链路调用

--- seata-order-service2001/src/main/java/com/learn/cloud/service/AccountService.java
+++ seata-order-service2001/src/main/java/com/learn/cloud/service/AccountService.java
@@ -1,11 +1,25 @@
 package com.learn.cloud.service;
 
+import com.learn.cloud.domain.CommonResult;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.math.BigDecimal;
 
 /**
  * @author xu.rb
  * @since 2020-04-28 15:37
  */
-@FeignClient("seata-account-service")
+@FeignClient(value = "seata-account-service")
 public interface AccountService {
+
+    /**
+     * 减余额
+     * @param userId
+     * @param money
+     * @return
+     */
+    @PostMapping(value = "/account/decrease")
+    CommonResult decrease(@RequestParam("userId") Long userId, @RequestParam("money") BigDecimal money);
 }
--- seata-order-service2001/src/main/java/com/learn/cloud/service/StorageService.java
+++ seata-order-service2001/src/main/java/com/learn/cloud/service/StorageService.java
@@ -1,12 +1,23 @@
 package com.learn.cloud.service;
 
+import com.learn.cloud.domain.CommonResult;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author xu.rb
  * @since 2020-04-28 15:37
  */
-@FeignClient("seata-storage-service")
+@FeignClient(value = "seata-storage-service")
 public interface StorageService {
 
+    /**
+     * 减库存
+     * @param productId
+     * @param count
+     * @return
+     */
+    @PostMapping(value = "/storage/decrease")
+    CommonResult decrease(@RequestParam("productId") Long productId, @RequestParam("count") Integer count);
 }
